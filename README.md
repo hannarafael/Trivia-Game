bd41503e92aa59a92d7722b5d6a0bb674899d041
hanna rafael <hannanrafael@gmail.com>
2019-10-06 23:38:51 -0400

    changes added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Trivia-Game
+Trivia game to demonstrate timer usage in javascript. Questions time out if the player does not answer in 30 seconds. Good luck!
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 //Initial values
-let counter = 5;
+let counter = 30;
 let currentQuestion= 0;
 let score = 0;
 let lost = 0;
@@ -15,6 +15,7 @@ const isQuestionOver = (quizQuestions.length - 1) === currentQuestion;
 if (isQuestionOver) {
     //TODO
     console.log("GAME OVER!!");
+    displayResult();
 } else {
     currentQuestion++;
     loadQuestion();
@@ -29,8 +30,8 @@ function timeUp() {
     clearInterval(timer);
 
     lost++;
-
-    nextQuestion();
+    preLoadImage('lost');
+    setTimeout(nextQuestion, 3 * 1000);
 }
 
 
@@ -47,17 +48,12 @@ function countDown () {
 
 
 }
-
-
-
-
-
-
 //Display the qestion and the choices to the browser 
+
 
 function loadQuestion() {
 
-    counter= 5;
+    counter= 30;
     timer = setInterval(countDown, 1000);
 
     const question = quizQuestions [currentQuestion].question;//
@@ -68,10 +64,11 @@ function loadQuestion() {
     $("#game").html(`
     <h4>${question}</h4>
     ${loadChoices(choices)}
+    ${loadRemainingQuestion()}
     `);
 
 }
-
+// loop over every choice starting at 0 
 function loadChoices(choices){
     let result = '';
     
@@ -79,15 +76,95 @@ function loadChoices(choices){
 
         result += `<p class="choice" data-answer="${choices[i]}">${choices[i]}</p>`;
     }
+    
     return result;
 
 
 }
 
 //Either correct or wrong answer, go to next question. 
+//event delegation
+$(document).on('click', '.choice', function() {
+    clearInterval(timer);
+    const selectedAnswer = $(this).attr('data-answer');
+    const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+
+    if (correctAnswer=== selectedAnswer) {
+     //TO DO 
+     // User wins
+     score++;
+     console.log('wins!');
+     preLoadImage('win');
+     setTimeout(nextQuestion, 3* 1000);
+    } else {
+        lost++;
+        
+        console.log('lost!');
+        preLoadImage('lost');
+        setTimeout(nextQuestion, 3 * 1000);
+    }
+
+});;
+
+function displayResult(){
+   const result= `
+     <p> You got ${score} question(s) right </p>
+     <p> You missed ${lost} question(s)</p>
+     <p> Total Questions ${quizQuestions.length} question(s) </p>
+     <button class="btn primary" id="reset">Reset Game</button>
+   
+   
+   `;
+    $ ('#game').html(result);
 
 
-loadQuestion();
+}
+
+$(document).on('click', '#reset', function(){
+  counter= 5;
+  currentQuestion= 0;
+  score= 0;
+  lost= 0;
+  timer= null;
+
+  loadQuestion();
+
+});;
+
+function loadRemainingQuestion(){
+     const remainingQuestion = quizQuestions.length - (currentQuestion + 1);
+     const totalQuestion = quizQuestions.length;
+
+     return `Remaining Question: ${remainingQuestion}/${totalQuestion}`;
+
+
+}
+
+//DISPLAY GIPHY FOR CORRECT OR WRONG ANSWER
+function preLoadImage(status){
+    const correctAnswer = quizQuestions[currentQuestion].correctAnswer;
+
+    if (status === 'win') {
+        $('#game').html(`
+        <p class ="preload-image"> Congrats, you got the right answer!</p>
+        <p class="preload-image"> The correct answer is${correctAnswer}</p>
+        `);
+    } else {
+        $('#game').html(`
+        <p class ="preload-image"> the correct answer was ${correctAnswer}</p>
+        <p class="preload-image"> You lost!</p>
+        `);
+    }
+}
+
+
+$('#start').click(function(){
+
+    $('#start').remove();
+    $('#time').html(counter);
+    loadQuestion();
+});;
+
 
 
 
